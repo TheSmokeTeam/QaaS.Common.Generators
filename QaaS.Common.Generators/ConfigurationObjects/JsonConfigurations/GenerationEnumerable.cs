f16825abded0060cadd78d1889defef753234af5
eldarush <eldarush@users.noreply.github.com>
2026-03-25 17:43:34 +0200

    fix(generators): clean solution build warnings

--- QaaS.Common.Generators/ConfigurationObjects/JsonConfigurations/GenerationEnumerable.cs
+++ QaaS.Common.Generators/ConfigurationObjects/JsonConfigurations/GenerationEnumerable.cs
@@ -30,7 +30,7 @@ public record GenerationEnumerable
 
     /// <summary>
     /// Retrieves the next record in the collection. If the current index is at the end of the collection and the provided policy is false,
-    /// a new Data<object> with a null body is returned. Otherwise, the index is reset to 0 and the first record in the collection is returned.
+    /// a new <c>Data&lt;object&gt;</c> with a null body is returned. Otherwise, the index is reset to 0 and the first record in the collection is returned.
     /// </summary>
     /// <param name="generationOutOfRangeLoopPolicy">The policy to handle the end of the collection.</param>
     /// <returns>The next record in the collection.</returns>
